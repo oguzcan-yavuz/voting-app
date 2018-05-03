c7316934818c52cdf0e9f6d079bf84517fb77de1
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-05-03 17:34:27 +0300

    sessions and passport github authentication implemented to the routes and server.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,39 @@
 const express = require('express');
 let router = express.Router();
 
-// main page router
-router.get('/', (req, res) => {
-    res.render('index');
-});
+module.exports = (passport) => {
 
-module.exports = router;
+    function isLoggedIn(req, res, next) {
+        if(req.isAuthenticated())
+            return next();
+        else
+            res.redirect('/login');
+    }
+
+    // home
+    router.get('/', isLoggedIn, (req, res) => {
+        res.render('index');
+    });
+
+    // login
+    router.get('/login', (req, res) => {
+        res.render('login');
+    });
+
+    // logout
+    router.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/login');
+    });
+
+    // authenticate github login with passport
+    router.get('/auth/github', passport.authenticate('github'));
+
+    // redirect after authenticate
+    router.get('/auth/github/callback', passport.authenticate('github', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
+    return router;
+};
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 require('./app/models/db.js').createConnection();   // create db connection
+const session = require('./app/config/session');
+const passport = require('./app/config/passport.js');
 const router = require('./app/routes/index.js');
 const PORT = process.env.PORT || 8080;
 let app = express();
@@ -13,7 +15,10 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
-app.use(router);
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(router(passport));
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT);
 });
