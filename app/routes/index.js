775f268db9ed8ffd848d6d6e4f6e942c84af8425
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-05-04 18:15:47 +0300

    new controllers integrated to routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const pollsController = require('../controllers/pollsController.js');
 const usersController = require('../controllers/usersController.js');
+let UsersController;
 let router = express.Router();
 
 
 module.exports = (passport) => {
-
     function isLoggedIn(req, res, next) {
         if(req.isAuthenticated())
             return next();
@@ -17,11 +17,27 @@ module.exports = (passport) => {
 
     // home
     router.get('/', async (req, res) => {
-        let users = await usersController.getUsers();
         let polls = await pollsController.getPolls();
+        let users, currentUser, ownedPolls, isVoted, newPoll;
+        if(req.isAuthenticated()) {
+            // newPoll = await UsersController.createPoll("I guess sixth poll created from UsersController", "it works", "it doesn't works", "no idea");
+            users = await UsersController.getAllUsers;
+            currentUser = UsersController.getCurrentUser;
+            ownedPolls = UsersController.getOwnedPolls;
+            isVoted = UsersController.isVoted(polls[0]);
+        }
+        let renderVars = {
+            polls: polls,
+            authenticated: req.isAuthenticated(),
+            username: (currentUser) ? currentUser.github.username : null
+        };
+        console.log("newPoll:", newPoll);
         console.log('users:', users);
         console.log("polls:", polls);
-        res.render('index', { polls: polls, authenticated: req.isAuthenticated() });
+        console.log("current user:", currentUser);
+        console.log("owned polls:", ownedPolls);
+        console.log("isVoted:", isVoted);
+        res.render('index', renderVars);
     });
 
     // logout
@@ -30,14 +46,33 @@ module.exports = (passport) => {
         res.redirect('/');
     });
 
+    // profile
+    router.get('/profile', isLoggedIn, (req, res) => {
+        let currentUser = UsersController.getCurrentUser;
+        res.render('profile', { user: currentUser });
+    });
+
+    // poll details
+    router.get('/polls/:pollID', async (req, res) => {
+        pollsController.getPoll(req.params.pollID)
+            .then(poll => {
+                res.render('pollDetails', { poll: poll });
+            })
+            .catch(err => {
+                res.redirect('/');
+            })
+    });
+
     // authenticate github login with passport
     router.get('/auth/github', passport.authenticate('github'));
 
     // redirect after authenticate
-    router.get('/auth/github/callback', passport.authenticate('github', {
-        successRedirect: '/',
-        failureRedirect: '/'
-    }));
+    router.get('/auth/github/callback', passport.authenticate('github'), (req, res) => {
+        // initialize UsersController with the logged in user if authentication is successful
+        if(req.isAuthenticated())
+            UsersController = new usersController(req.user);
+        res.redirect('/');
+    });
 
     return router;
 };
